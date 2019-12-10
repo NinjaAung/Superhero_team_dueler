7a46cdc1d0691a4b17248eca0e0d5f10998e4e67
ninjaaung <ninja.aung02@gmail.com>
2019-12-10 03:52:31 -0800

    Added Pytest for arena

--- superhero.py
+++ superhero.py
@@ -27,7 +27,7 @@ class Armor:
         ''' Return a random value between 0 and the initialized max_block strength. '''
         return random.randint(0, self.max_block)
 
-class Hero(Ability, Armor):
+class Hero:
     def __init__(self, name, starting_health=100):
         '''Instance properties:
           abilities: List
@@ -37,8 +37,9 @@ class Hero(Ability, Armor):
           current_health: Integer
         '''
         self.name = name
+        self.starting_health = starting_health
         self.max_health = starting_health
-        self.current_health = self.max_health
+        self.current_health = starting_health
         self.armors = list()
         self.abilities = list()
         self.kills = 0
@@ -47,6 +48,11 @@ class Hero(Ability, Armor):
     def add_ability(self, ability):
         ''' Add ability to abilities list '''
         self.abilities.append(ability)
+
+    def add_weapon(self, weapon):
+        '''Add weapon to self.abilities'''
+        self.abilities.append(weapon)
+        
 
     def attack(self):
         '''Calculate the total damage from all ability attacks.
@@ -57,7 +63,6 @@ class Hero(Ability, Armor):
         for ability in self.abilities: 
             new_damage = damage + ability.attack()
             damage = new_damage
-
         return damage
 
     def add_armor(self, armor):
@@ -86,7 +91,7 @@ class Hero(Ability, Armor):
             damage_taken = 0
 
         self.current_health = self.current_health - damage_taken
-         
+
         return self.current_health
 
     def is_alive(self):  
@@ -112,24 +117,23 @@ class Hero(Ability, Armor):
         if self.abilities == [] and opponent.abilities == []:
             print("Draw!") 
 
-        while self.is_alive == True and opponent.is_alive == True:
-            self.take_damage(opponent.attack)
-            opponent.take_damage(self.attack)
+        while self.is_alive() == True and opponent.is_alive() == True:
+            self.take_damage(opponent.attack())
+            opponent.take_damage(self.attack())
             
-        if self.is_alive() == True:
+        if self.is_alive() == True and opponent.is_alive() == False:
             print(f"{self.name} has Won!")
             self.add_kill()
             opponent.add_death()
-        elif opponent.is_alive == True:
+        elif opponent.is_alive == True and self.is_alive() == False:
             print(f"{opponent.name} has Won!")
             opponent.add_kill()
             self.add_death()
         else:
             print("Both Players have died. It is a draw!")
             self.add_death()
-            opponent.add_death()
+            opponent.add_death()            
 
-            
 class Weapon(Ability):
     def attack(self):
         '''  This method returns a random value
@@ -205,10 +209,118 @@ class Team(Hero, Ability):
             print(f'''    {hero.name}: 
             kills: {hero.kills}
             deaths: {hero.deaths}
+            k/d: {hero.kills//hero.deaths}
             ''')
 
+class Arena(Ability):
+    def __init__(self):
+        '''Instantiate properties
+        team_one: None
+        team_two: None
+        '''
+        self.team_one = None
+        self.team_two = None
 
+    def create_ability(self):
+        '''Prompt for Ability information.
+            return Ability with values from user Input
+        '''
+        ability = Ability(input_handler("What is the name of the ability? "), input_handler("Give a strength value: "))
+        return ability
+
+    def create_weapon(self):
+        '''Prompt user for Weapon information
+            return Weapon with values from user input.
+        '''
+        weapon = Weapon(input_handler("What is the name of the Weapon? "), input_handler("Give a strength value: "))
+        return weapon
+
+    def create_armor(self):
+        '''Prompt user for Armor information
+          return Armor with values from user input.
+        '''
+        armor = Armor(input_handler("What is the name of the armor? "), input_handler("Give a strength value: "))
+        return armor
         
+    def create_hero(self):
+        '''Prompt user for Hero information
+          return Hero with values from user input.
+        '''
+        hero = Hero(input_handler("What is the name of your hero? "))
+
+        number = int(input_handler("how many abilities do you want?"))
+        for _ in range(number):
+            ability = self.create_ability()
+            hero.add_ability(ability)
+
+        number = int(input_handler("how many pieces of armor do you want?"))
+        for _ in range(number):
+            armor = self.create_armor()
+            hero.add_armor(armor)
+
+        number = int(input_handler("how many weapons do you want?"))
+        for _ in range(number):
+            weapon = self.create_weapon()
+            hero.add_ability(weapon)
+
+        return hero
+
+    def add_hero_team(self, team):
+        # create_hero()
+        team.add_hero(self.create_hero())
+
+        yes_no = input_handler("Do you want to add more heroes? ")
+        if yes_no.lower() == "yes":
+            return self.add_hero_team(team)
+
+    def build_team_one(self):
+        '''Prompt the user to build team_one '''
+        print("lets build the team now! ")
+        self.team_one = Team(input_handler("What is your team name? "))
+        self.add_hero_team(self.team_one)
+
+        return self.team_one
+
+    def build_team_two(self):
+        '''Prompt the user to build team_two '''
+        print("lets build the team now! ")
+        self.team_two = Team(input_handler("What is your team name? "))
+        self.add_hero_team(self.team_two)
+
+        return self.team_two
+
+    def team_battle(self):
+        '''Battle team_one and team_two together.'''
+        # TODO: This method should battle the teams together.
+        # Call the attack method that exists in your team objects
+        # for that battle functionality.
+        self.team_one = self.build_team_one()
+        self.team_two = self.build_team_two()
+
+        return self.team_one.attack(self.team_two)
+    
+    def show_stats(self):
+        '''Prints team statistics to terminal.'''
+        self.team_one.team_won(self.team_two)
+        print(self.team_one.stats)
+        print(self.team_two.stats)
+            
+        
+
+    
+
+
+def input_handler(prompt):
+    user_input = input(prompt)
+
+    if user_input == '':
+        print('you must provide some information!')
+        return input_handler(prompt)
+    elif prompt == "Give a strength value: " and user_input.isalpha() == True:
+        print('You must input a number!')
+        return input_handler(prompt)
+
+    return user_input
 
 
 
