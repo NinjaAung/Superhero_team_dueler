c5601df691a69775b09e7cfc560f749ad6701614
ninjaaung <ninja.aung02@gmail.com>
2019-12-10 03:48:22 -0800

    Corrected Test Function and team functanality working

--- superhero.py
+++ superhero.py
@@ -41,6 +41,8 @@ class Hero(Ability, Armor):
         self.current_health = self.max_health
         self.armors = list()
         self.abilities = list()
+        self.kills = 0
+        self.deaths = 0
 
     def add_ability(self, ability):
         ''' Add ability to abilities list '''
@@ -94,8 +96,116 @@ class Hero(Ability, Armor):
             return False
         else:
             return True
-        pass
 
+    def add_kill(self):
+        ''' Adds 1 to the amount of kills the hero has '''
+        self.kills += 1
+
+    def add_death(self):
+        ''' adds 1 to the amount of deaths the hero has '''
+        self.deaths += 1
+        
+
+    def fight(self, opponent):  
+        ''' Current Hero will take turns fighting the opponent hero passed in.
+        '''
+        if self.abilities == [] and opponent.abilities == []:
+            print("Draw!") 
+
+        while self.is_alive == True and opponent.is_alive == True:
+            self.take_damage(opponent.attack)
+            opponent.take_damage(self.attack)
+            
+        if self.is_alive() == True:
+            print(f"{self.name} has Won!")
+            self.add_kill()
+            opponent.add_death()
+        elif opponent.is_alive == True:
+            print(f"{opponent.name} has Won!")
+            opponent.add_kill()
+            self.add_death()
+        else:
+            print("Both Players have died. It is a draw!")
+            self.add_death()
+            opponent.add_death()
+
+            
+class Weapon(Ability):
+    def attack(self):
+        '''  This method returns a random value
+        between one half to the full attack power of the weapon.
+        '''
+        return random.randint(self.max_damage//2, self.max_damage)
+
+class Team(Hero, Ability):
+    def __init__(self, name):
+        ''' Initialize your team with its team name
+        '''
+        self.name = name
+        self.heroes = list()
+        
+    def remove_hero(self, name):
+        '''Remove hero from heroes list.
+        If Hero isn't found return 0.
+        '''
+        # return self.heroes.remove(name) if name in self.heroes else 0 
+        if name in self.heroes:
+            return self.heroes.remove(name)
+        else:
+            return 0
+    
+    def view_all_heroes(self):
+        print('These are your heroes: ')
+        for hero in self.heroes:
+            print(" --       ", hero.name)
+        
+
+    def add_hero(self, hero):
+        '''Add Hero object to self.heroes.'''
+        
+        self.heroes.append(hero)
+
+    def team_kills(self):
+        total_kills = 0
+
+        for hero in self.heroes:
+            kill = hero.kills + total_kills
+            total_kills = kill
+
+        return total_kills
+
+    def attack(self, other_team):
+        ''' Battle each team against each other.'''
+        heroes_fought = list()
+        while len(heroes_fought) != len(self.heroes) + len(other_team.heroes):
+            
+            for hero1, hero2 in zip(self.heroes, other_team.heroes):
+                hero1.fight(hero2)
+                heroes_fought.append(hero1)
+                heroes_fought.append(hero2)
+    
+    def team_won(self, other_team):
+        if self.team_kills() > other_team.team_kills():
+            print(f"{self.name} has won!")
+        elif self.team_kills() < other_team.team_kills():
+            print(f"{other_team.name} has won!")
+        else:
+            print("draw!!")
+
+    def revive_heroes(self, health=100):
+        ''' Reset all heroes health to starting_health'''
+        for hero in self.heroes:
+            hero.current_health = health
+    
+
+    def stats(self):
+        '''Print team statistics'''
+        print("Stats: ")
+        for hero in self.heroes:
+            print(f'''    {hero.name}: 
+            kills: {hero.kills}
+            deaths: {hero.deaths}
+            ''')
 
 
         
